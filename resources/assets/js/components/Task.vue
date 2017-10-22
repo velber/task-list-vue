2d647d1915f6eae5f3bfc0645276e1e2e05cc2fb
webler <wolodymyr.ch@gmail.com>
2017-10-22 21:28:04 +0300

    plug get-mdl

--- resources/assets/js/components/Task.vue
+++ resources/assets/js/components/Task.vue
@@ -1,14 +1,43 @@
 <template>
-    <div>
-        {{ task.name }}
-    </div>
+    <li class="mdl-list__item">
+            <span>
+                <button class="mdl-button mdl-js-button mdl-button--icon">
+                    <i class="material-icons">blur_on</i>
+                </button>
+            </span>
+        <span class="mdl-list__item-check">
+                <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" :for="getCheckboxId(task.id)"
+                       style="width: 30px;">
+                   <input type="checkbox" :id="getCheckboxId(task.id)" class="mdl-checkbox__input" checked/>
+                </label>
+            </span>
+        <span class="mdl-list__item-primary-content">
+            {{ task.name }}
+        </span>
+        <span class="mdl-list__item-secondary-action">
+                <button class="mdl-button mdl-js-button mdl-button--icon">
+                    <i class="material-icons">cancel</i>
+                </button>
+        </span>
+    </li>
 </template>
 
 <script>
     export default {
         props: ['task'],
+        methods: {
+            getCheckboxId(id) {
+                return `list-checkbox-${id}`;
+            }
+        },
         mounted() {
-            console.log(this.task);
+            //console.log(this.task);
         }
     }
 </script>
+
+<style>
+    .mdl-list__item-check {
+        padding-left: 5px;
+    }
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('main-content')
     <div id="app">
         <h1 class="text-center">To Do List</h1>
-        <ul>
+        <ul class="demo-list-control mdl-list">
             <task v-for="(task, key, index) in tasks" :key="task.id" :task="task"></task>
         </ul>
     </div>
